9f6affbf79c40ca3c9bf30ad9c3dc3d432f55a73
Kaffeekautz <kaffeekautz@gmail.com>
2023-12-04 15:42:22 +0100

    add comments

--- 04-12-23/4-12-23-P1.py
+++ 04-12-23/4-12-23-P1.py
@@ -1,7 +1,9 @@
 def calculate_points(winning_numbers, your_numbers):
+    # Find the numbers that are both in winning_numbers and your_numbers
     matches = [num for num in your_numbers if num in winning_numbers]
     matches_count = len(matches)
 
+    # Display the matches for this card
     if matches_count == 1:
         print(f"Matches: [{matches[0]}]")
         return 1
@@ -13,6 +15,7 @@ def calculate_points(winning_numbers, your_numbers):
         return 0
 
 def main():
+    # Read lines from the input file
     with open("04-12-23/input.txt", "r") as file:
         lines = file.readlines()
 
@@ -32,13 +35,16 @@ def main():
         numbers_str = parts[1].strip()
 
         try:
+            # Split the numbers into winning_numbers and your_numbers
             your_numbers_str, winning_numbers_str = numbers_str.split("|")
             winning_numbers = sorted(list(map(int, winning_numbers_str.split())))
             your_numbers = sorted(list(map(int, your_numbers_str.split())))
 
+            # Calculate and display points for the current card
             card_points = calculate_points(winning_numbers, your_numbers)
             total_points += card_points
 
+            # Store card information for later sorting and display
             card_info.append((int(card_number.split()[1]), f"Card {card_number}", card_points, your_numbers, winning_numbers))
         except ValueError:
             print(f"Skipping invalid line: {line}")
@@ -46,6 +52,7 @@ def main():
     # Sort the card_info list by card number
     card_info.sort(key=lambda x: x[0])
 
+    # Display information for each card in sorted order
     for _, card_number, points, your_numbers, winning_numbers in card_info:
         print(f"{card_number}: {points} Points")
         print("Left side: ", your_numbers)
